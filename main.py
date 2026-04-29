3f273d02274c5a50cfc7fbf6acfe34929bc1215b
22062 <22062@students.mrgs.school.nz>
2026-04-30 09:51:02 +1200

    modified...

--- main.py
+++ main.py
@@ -1,40 +1,12 @@
 from tkinter import*
 root = Tk()
-root.geometry("600x400")
+root.geometry("700x400")
+root.resizable(False,False)
 
+myLabel = Label(root, text="welcome to the rank score calculator")
+myLabel.pack()
 
-#Create home screen
-class Intro:
-    def __init__(self, parent):
-     background_color= "black"
-     self.intro_frame = Frame(parent, bg=background_color)
-     self.intro_grid = ("nsew")
+myButton = Button(root, text="click to start calculating")
+myButton.pack()
 
-     self.top_label = Label(self.intro_frame, text="hi")
-
-
-
-class Calculation:
-    def __init__(self, parent):
-     background_color= "white"
-     self.calculation_frame = Frame(parent, bg=background_color)
-     self.calculation_grid = ("nsew")
-
-
-
-class Final:
-    def __init__(self, parent):
-     background_color= "white"
-     self.final_frame = Frame(parent, bg=background_color)
-     self.final_grid = ("nsew")
-
-
-
-
-
-
-#Launches the home screen
-if __name__ == "__main__":
-    root.title("Rank Score Calculator")
-    program_instance = Intro(root)
-    root.mainloop()
+root.mainloop()
