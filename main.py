0c0e28e2cc0665df45bead217b613a75db639a34
22062 <22062@students.mrgs.school.nz>
2026-05-01 14:40:58 +1200

    Added base code for user entry

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ root = Tk()
 root.geometry("700x400")
 root.resizable(False,False)
 
-myLabel = Label(root, text="welcome to the rank score calculator")
+e = Entry(root, width=50)
+e.pack()
+
+def myClick():
+    username = "Enter your username" + e.get()
+    myLabel = Label(root, text="asd" )
+    myLabel.pack()
+
+myLabel = Label(root, text="welcome to the rank score calculator",)
 myLabel.pack()
 
 myButton = Button(root, text="click to start calculating")
