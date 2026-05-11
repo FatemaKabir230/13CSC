39558bd663d6ba5be58e06e17eead0a9cfb1f8ee
22062 <22062@students.mrgs.school.nz>
2026-05-11 13:26:20 +1200

    Added username entry

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ root.geometry("700x400")
 root.resizable(False,False)
 
 
-root = tkinter.Tk
-frame = tkinter.Frame(root)
+frame = Frame(root)
 frame.pack()
 
 e = Entry(root, width=50)
 e.pack()
 e.insert(0, "Enter your username")
+
+
+myLabel = Label(root, text="welcome to the rank score calculator")
+myLabel_frame.grid(row=1, column=1)
+
 
 def username_data():
     username = username_entry.get()
@@ -20,17 +24,16 @@ def username_data():
 username_info_frame = tkinter.LabelFrame(frame, text="Enter your username")
 username_info_frame.grid(row=1, column=2, padx=5, pady=5)
 
+info_button = tkinter.LabelFrame(frame, text="Click here for program information")
+info_button.grid(row=5, column=1)
+
 username_entry =  tkinter.Entry(frame, text="Username:")
 username_entry.grid(row=1, column=2, padx=5, pady=5)
 
 
 def myClick():
-    myLabel = Label(root, text="Enter your username here:")
-    MyLabel = "Username:" + e.get()
+    myLabel = Label(root, text="Enter your username here:" + e.get())
     myLabel.pack()
-
-myLabel = Label(root, text="welcome to the rank score calculator", command=myClick)
-myLabel.pack()
 
 myButton = Button(root, text="click to start calculating")
 myButton.pack()
