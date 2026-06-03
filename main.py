48656b2b386b71083be2585628ba21fca3dd75c2
22062 <22062@students.mrgs.school.nz>
2026-06-04 09:53:49 +1200

    Minor changes

--- main.py
+++ main.py
@@ -98,55 +98,60 @@ def submit_username():
     dropdown5 = OptionMenu(new_class, selected_option, *subjects)
     dropdown5.place(relx=0.04, rely=0.8, anchor="center")
 
-    s1e = tkinter.Entry(new_class, width=20)
-    s1e.place(relx=0.2, rely=0.2, anchor="center")
-    s1e = tkinter.Entry(new_class)
-    s1e.place(relx=0.2, rely=0.35, anchor="center")
-    s1e = tkinter.Entry(new_class)
-    s1e.place(relx=0.2, rely=0.5, anchor="center")
-    s1e = tkinter.Entry(new_class)
-    s1e.place(relx=0.2, rely=0.65, anchor="center")
-    s1e = tkinter.Entry(new_class)
-    s1e.place(relx=0.2, rely=0.8, anchor="center")
+    #Subject and credit entry widgets
+    s1na = tkinter.Entry(new_class, width=20)
+    s1na.place(relx=0.2, rely=0.2, anchor="center")
+    s2na = tkinter.Entry(new_class)
+    s2na.place(relx=0.2, rely=0.35, anchor="center")
+    s3na = tkinter.Entry(new_class)
+    s3na.place(relx=0.2, rely=0.5, anchor="center")
+    s4na = tkinter.Entry(new_class)
+    s4na.place(relx=0.2, rely=0.65, anchor="center")
+    s5na = tkinter.Entry(new_class)
+    s5na.place(relx=0.2, rely=0.8, anchor="center")
 
-    s2e = tkinter.Entry(new_class)
-    s2e.place(relx=0.4, rely=0.2, anchor="center")
-    s2e = tkinter.Entry(new_class)
-    s2e.place(relx=0.4, rely=0.35, anchor="center")
-    s2e = tkinter.Entry(new_class)
-    s2e.place(relx=0.4, rely=0.5, anchor="center")
-    s2e = tkinter.Entry(new_class)
-    s2e.place(relx=0.4, rely=0.65, anchor="center")
-    s2e = tkinter.Entry(new_class)
-    s2e.place(relx=0.4, rely=0.8, anchor="center")
+    s1a = tkinter.Entry(new_class)
+    s1a.place(relx=0.4, rely=0.2, anchor="center")
+    s2a = tkinter.Entry(new_class)
+    s2a.place(relx=0.4, rely=0.35, anchor="center")
+    s3a = tkinter.Entry(new_class)
+    s3a.place(relx=0.4, rely=0.5, anchor="center")
+    s4a = tkinter.Entry(new_class)
+    s4a.place(relx=0.4, rely=0.65, anchor="center")
+    s5a = tkinter.Entry(new_class)
+    s5a.place(relx=0.4, rely=0.8, anchor="center")
 
-    s3e = tkinter.Entry(new_class)
-    s3e.place(relx=0.6, rely=0.2, anchor="center")
-    s3e = tkinter.Entry(new_class)
-    s3e.place(relx=0.6, rely=0.35, anchor="center")
-    s3e = tkinter.Entry(new_class)
-    s3e.place(relx=0.6, rely=0.5, anchor="center")
-    s3e = tkinter.Entry(new_class)
-    s3e.place(relx=0.6, rely=0.65, anchor="center")
-    s3e = tkinter.Entry(new_class)
-    s3e.place(relx=0.6, rely=0.8, anchor="center")
+    s1m = tkinter.Entry(new_class)
+    s1m.place(relx=0.6, rely=0.2, anchor="center")
+    s2m = tkinter.Entry(new_class)
+    s2m.place(relx=0.6, rely=0.35, anchor="center")
+    s3m = tkinter.Entry(new_class)
+    s3m.place(relx=0.6, rely=0.5, anchor="center")
+    s4m = tkinter.Entry(new_class)
+    s4m.place(relx=0.6, rely=0.65, anchor="center")
+    s5m = tkinter.Entry(new_class)
+    s5m.place(relx=0.6, rely=0.8, anchor="center")
 
-    s4e = tkinter.Entry(new_class)
-    s4e.place(relx=0.8, rely=0.2, anchor="center")
-    s4e = tkinter.Entry(new_class)
-    s4e.place(relx=0.8, rely=0.35, anchor="center")
-    s4e = tkinter.Entry(new_class)
-    s4e.place(relx=0.8, rely=0.5, anchor="center")
+    s1e = tkinter.Entry(new_class)
+    s1e.place(relx=0.8, rely=0.2, anchor="center")
+    s2e = tkinter.Entry(new_class)
+    s2e.place(relx=0.8, rely=0.35, anchor="center")
+    s3e = tkinter.Entry(new_class)
+    s3e.place(relx=0.8, rely=0.5, anchor="center")
     s4e = tkinter.Entry(new_class)
     s4e.place(relx=0.8, rely=0.65, anchor="center")
-    s4e = tkinter.Entry(new_class, )
-    s4e.place(relx=0.8, rely=0.8, anchor="center")
+    s5e = tkinter.Entry(new_class, )
+    s5e.place(relx=0.8, rely=0.8, anchor="center")
+
+    #focus on certain entry widgets?
+    s4e.focus()
 
     def final_window():
         results_window = Toplevel()
-        results_window.title("Entry of Subjects/Credits")
+        results_window.title("Rank Score Results and Desired Degree")
         results_window.configure(bg="#CEECF6")
         results_window.resizable(True, True)
+        results_window.geometry("700x400    ")
 
         frame = Frame(results_window, bg="#CEECF6")
         frame.place(relwidth=1, relheight=1)
@@ -159,9 +164,9 @@ def submit_username():
         total_rank_score.place(relx=0.5, rely=0.1, anchor="center")
 
         # Breakdown of total credits of each grade
-        not_achieved_credits = Label(results_window, text="NA:", bg="#CEECF6")
+        not_achieved_credits = Label(results_window, text="NOT ACHIEVED:", bg="#CEECF6")
         not_achieved_credits.place(relx=0.15, rely=0.1, anchor="center")
-        achieved_credits = Label(results_window, text="ACH:", bg="#CEECF6")
+        achieved_credits = Label(results_window, text="ACHIEVED:", bg="#CEECF6")
         achieved_credits.place(relx=0.35, rely=0.1, anchor="center")
         merit_credits = Label(results_window, text="MERIT:", bg="#CEECF6")
         merit_credits.place(relx=0.55, rely=0.1, anchor="center")
@@ -176,9 +181,18 @@ def submit_username():
         user_rank_score = Label(results_window, text="#:", bg="#CEECF6")
         user_rank_score.place(relx=0.15, rely=0.3, anchor="center")
 
+        # Label to say whether they got their chosen rank score or not
+        comparision_label = Label(results_window, text="Your rank score is X lower/higher than chosen rank score", bg="#CEECF6")
+        comparision_label.place(relx=0.5, rely=0.5, anchor="center", )
+
+        #Code to meet desired rank score
+
+
+
         #Dictionary of UOA undergrad degrees with assigned value as needed rank score
+        #pay attention to changed degree rank scores (biomed and health sci) + new degrees like science field - cells etc
         UOA_undergrad_degrees= { ["Bachelor of Commerce (BCom)", "200"], ["Bachelor of Property (BProp)", "165"], ["Bachelor of Early Childhood Studies (BECSt)", "150"]
-        ["Bachelor of Education (Tchg)", "150",],["Bachelor of Education (TESOL)", "150"], ["Bachelor of Architectural Studies (BAS)", "230"],
+        ["Bachelor of Education (Tchg)", "150"],["Bachelor of Education (TESOL)", "150"], ["Bachelor of Architectural Studies (BAS)", "230"],
         ["Bachelor of Design (BDes)", "180"], ["Bachelor of Engineering (Honours) (BE(Hons))", "250"], ["Bachelor of Urban Planning(Honours) (BUrbPlan(Hons))", "180"],
         ["Bachelor of Health Sciences(BHSc)", "250"], ["Bachelor of Nursing (BNurs)", "230"], ["Bachelor of Sport Health and Physical Education (BSportHPE)", "150"],
         ["Bachelor of Arts (BA)", "150"], ["Bachelor of Communication (BC)", "165"], ["Bachelor of Global Studies (BGlobalSt)", "180"], ["Bachelor of Global Studies (BGlobalSt)", "180"],
@@ -188,15 +202,20 @@ def submit_username():
 
 
 #degrees which require to finish bachelor of science first year then - Bachelor of Medical Imaging (Honours) (BMedImag(Hons)),
-#Bachelor of Medicine and Bachelor of Surgery (MBChB)
+#Bachelor of Medicine and Bachelor of Surgery (MBChB), optometry, and
         #Drop down menu for choosing UOA degree
         degree_selected_option = StringVar()
-        degree_selected_option.set("Select your desired degree of admission")
+        degree_selected_option.set("Select your desired degree of admission, note that some degrees may be omitted due to having to qualify/finishing for a  BSc first")
         degree_1_dropdown = OptionMenu(results_window, degree_selected_option, *UOA_undergrad_degrees)
         degree_1_dropdown.place(relx=0.04, rely=0.2, anchor="center")
 
+    def calcrs():
+        s5e.get(), s4e.get(), s3e.get(), s2e.get(), s1e.get()
+        if
+
+
     # Entry page done button which when clicked launches results window, and destroys entry page
-    results_button = Button(new_class, text="Done", command=lambda: [final_window(), new_class.destroy()])
+    results_button = Button(new_class, text="Done", command=lambda: [final_window(), new_class.destroy(), calcrs()])
     results_button.place(relx=0.5, rely=0.8)
 
 #Home page submit button which when clicked launches entry page and destroys home page
